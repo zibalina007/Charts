8abefb3d69801c94d1929bc0f2d46ffc556ddc0e
Erik Campobadal <soc@erik.cat>
2016-10-02 12:32:50 +0200

    New material charts

---
+++ src/Templates/material.line.php
@@ -0,0 +1,36 @@
+<?php
+
+$graph = "
+    <script type='text/javascript'>
+    google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['', \"$this->element_label\"],
+          ";
+          for($i = 0; $i < count($this->values); $i++){
+              $e = $this->labels[$i];
+              $v = $this->values[$i];
+              $graph .= "[\"$e\", $v],";
+          }
+          $graph .= "
+        ]);
+
+        var options = {
+          chart: {
+            title: \"$this->title\",
+          },
+          ";
+          $graph .= $this->colors ? "colors: ['" . $this->colors[0] . "']" : "";
+          $graph .= "
+        };
+
+        var chart = new google.charts.Line(document.getElementById('$this->id'));
+
+        chart.draw(data, options);
+      }
+    </script>
+    <div "; $graph .= $this->responsive ? "" : "style='width: " . $this->width ."px; height: " . $this->height . "px;'"; $graph .= " id='$this->id'></div>
+";
+
+return $graph;
--- src/includes.php
+++ src/includes.php
@@ -5,7 +5,7 @@ return "
 
     <script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
     <script type='text/javascript' src='https://www.google.com/jsapi'></script>
-    <script type='text/javascript'>google.charts.load('current', {'packages':['corechart', 'geochart']});</script>
+    <script type='text/javascript'>google.charts.load('current', {'packages':['corechart', 'geochart', 'bar', 'line']});</script>
 
     <script src='".asset('/vendor/consoletvs/charts/highcharts/js/highcharts.js')."'></script>
     <script src='".asset('/vendor/consoletvs/charts/highcharts/js/modules/exporting.js')."'></script>
@@ -14,17 +14,17 @@ return "
     <script src='".asset('/vendor/consoletvs/charts/highmaps/maps/world.js')."'></script>
 
     <script src='".asset('/vendor/consoletvs/charts/chartjs/Chart.js')."'></script>
-	
+
     <script src='".asset('/vendor/consoletvs/charts/chartist/chartist.min.js')."'></script>
 	<link rel='stylesheet' type='text/css' href='".asset('/vendor/consoletvs/charts/chartist/chartist.min.css')."' />
-	
+
 	<script type='text/javascript' src='".asset('/vendor/consoletvs/charts/fusioncharts/fusioncharts.js')."'></script>
 	<script type='text/javascript' src='".asset('/vendor/consoletvs/charts/fusioncharts/themes/fusioncharts.theme.fint.js')."'></script>
-	
+
 	<link rel='stylesheet' href='".asset('/vendor/consoletvs/charts/morris/morris.css')."'>
 	<script type='text/javascript' src='".asset('/vendor/consoletvs/charts/morris/raphael.min.js')."'></script>
 	<script type='text/javascript' src='".asset('/vendor/consoletvs/charts/morris/morris.min.js')."'></script>
-	
+
 	<script src='".asset('/vendor/consoletvs/charts/d3/d3.js')."'></script>
 	<link rel='stylesheet' href='".asset('/vendor/consoletvs/charts/plottable/plottable.css')."'>
 	<script src='".asset('/vendor/consoletvs/charts/plottable/plottable.js')."'></script>
