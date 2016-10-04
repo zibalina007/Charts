e6b97b2f7d577af70300582ec93db7cf21badac2
Erik Campobadal <ConsoleTVs@gmail.com>
2016-10-04 19:51:46 +0200

    Update morris.line.php

--- src/Templates/morris.line.php
+++ src/Templates/morris.line.php
@@ -1,17 +1,21 @@
 <?php
 
-$graph = '
-<div '; if (!$this->responsive) {
-    $graph .= $this->width ? "style='width: ".$this->width."px'" : '';
+$graph = "
+<style>
+#".$this->id." > svg {
+	max-width: 100%;
+	max-height: 100%;
+}
+</style>
+<div "; if (!$this->responsive) {
+    $graph .= $this->width ? "style='width: " . $this->width . "px'" : "";
 } $graph .= "><center><b style='font-family: Arial, Helvetica, sans-serif;font-size: 18px;'>$this->title</b></center></div>
 	<div id='$this->id' "; if (!$this->responsive) {
     $graph .= "style='";
-    $graph .= $this->height ? 'height: '.$this->height.'px' : '';
-    $graph .= $this->width ? 'width: '.$this->width.'px' : '';
+    $graph .= $this->height ? "height: " . $this->height . "px;" : "";
+    $graph .= $this->width ? "width: " . $this->width . "px;" : "";
     $graph .= "'";
-} else {
-    $graph .= "style='height: 100%; width: 100%;'";
-} $graph .= " ></div>
+}$graph .= " ></div>
     <script type='text/javascript'>
 		Morris.Line({
 		  element: '$this->id',
