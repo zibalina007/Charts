3711cb41961acd30c43c81ad3d924ed451afc257
Erik Campobadal <ConsoleTVs@gmail.com>
2016-10-04 19:59:57 +0200

    Update morris.line.php

--- src/Templates/morris.line.php
+++ src/Templates/morris.line.php
@@ -1,46 +1,44 @@
 <?php
 
-$graph = "
-<style>
-#".$this->id." > svg {
-	max-width: 100%;
-	max-height: 100%;
-}
-</style>
-<div "; if (!$this->responsive) {
+$graph = '
+<div '; if (!$this->responsive) {
     $graph .= $this->width ? "style='width: " . $this->width . "px'" : "";
 } $graph .= "><center><b style='font-family: Arial, Helvetica, sans-serif;font-size: 18px;'>$this->title</b></center></div>
 	<div id='$this->id' "; if (!$this->responsive) {
     $graph .= "style='";
-    $graph .= $this->height ? "height: " . $this->height . "px;" : "";
-    $graph .= $this->width ? "width: " . $this->width . "px;" : "";
+    $graph .= $this->height ? "height: " . $this->height . "px" : "";
+    $graph .= $this->width ? "width: " . $this->width . "px" : "";
     $graph .= "'";
-}$graph .= " ></div>
+} else {
+    $graph .= "style='height: 100%; width: 100%;'";
+} $graph .= " ></div>
     <script type='text/javascript'>
-		Morris.Line({
-		  element: '$this->id',
-		  resize: true,
-		  data: [
-			";
-                $i = 0;
-                foreach ($this->values as $v) {
-                    $l = $this->labels[$i];
-                    $graph .= "{x: \"$l\", y: $v},";
-                    $i++;
-                }
-            $graph .= "
-		  ],
-		  xkey: 'x',
-		  ykeys: ['y'],
-		  labels: [\"$this->element_label\"],
-		  hideHover: 'auto',
-		  parseTime: false,
-		  ";
-            if ($this->colors) {
-                $graph .= 'lineColors: ["'.$this->colors[0].'"],';
-            }
-          $graph .= '
+		$(function (){
+			Morris.Line({
+			  element: '$this->id',
+			  resize: true,
+			  data: [
+				";
+					$i = 0;
+					foreach ($this->values as $v) {
+						$l = $this->labels[$i];
+						$graph .= "{x: \"$l\", y: $v},";
+						$i++;
+					}
+				$graph .= "
+			  ],
+			  xkey: 'x',
+			  ykeys: ['y'],
+			  labels: [\"$this->element_label\"],
+			  hideHover: 'auto',
+			  parseTime: false,
+			  ";
+				if ($this->colors) {
+					$graph .= 'lineColors: ["'.$this->colors[0].'"],';
+				}
+			  $graph .= '
 
+			});
 		});
     </script>
 ';
