4a09826cc89cafed4f0bc790bd4cc6c2065d7a1c
Emmy <ajemeke@gmail.com>
2016-10-26 00:13:12 +0100

    extending custom render ability to chartist lib

--- src/Templates/_partials/chartist1-container.php
+++ src/Templates/_partials/chartist1-container.php
@@ -1,11 +1,14 @@
 <?php
 
-$graph = '
+$graph .= '
 <div '; if (!$this->responsive) {
-    $graph .= "style='width: $this->width'";
+    $graph .= $this->width ? "style='width: ".$this->width."px'" : '';
 } $graph .= "><center><b style='font-family: Arial, Helvetica, sans-serif;font-size: 18px;'>$this->title</b></center></div>
 <div id='$this->id' "; if (!$this->responsive) {
-    $graph .= "style='max-height: $this->height; max-width: $this->width'";
+    $graph .= "style='";
+    $graph .= $this->height ? 'height: '.$this->height.'px;' : '';
+    $graph .= $this->width ? 'width: '.$this->width.'px;' : '';
+    $graph .= "' ";
 } $graph .= " class='ct-chart ct-perfect-fourth'></div>";
 
 ?>
