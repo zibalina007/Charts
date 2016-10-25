d2771d6a091a8b77be0da945abebc8c6b6efdd2f
Emmy <ajemeke@gmail.com>
2016-10-25 22:54:05 +0100

    fix no new line between libs assets

--- src/Charts.php
+++ src/Charts.php
@@ -147,7 +147,7 @@ class Charts extends Facade
                 // return all assets of type in requested libs
                 return collect($libs)->reduce(function ($result, $lib) use ($type, $includes) {
                     return (!empty($includes[$lib][$type]))
-                        ? $result.implode("\n", $includes[$lib][$type])
+                        ? $result . "\n" . implode("\n", $includes[$lib][$type])
                         : $result;
                 });
             }
@@ -155,7 +155,7 @@ class Charts extends Facade
             // return all libraries assets that match requested libraries
             return collect($libs)->reduce(function ($result, $lib) use ($includes) {
                 return (!empty($includes[$lib]))
-                    ? $result.implode("\n", array_flatten($includes[$lib]))
+                    ? $result . "\n" . implode("\n", array_flatten($includes[$lib]))
                     : $result;
             });
         }
