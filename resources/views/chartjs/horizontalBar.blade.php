92f928db2114c78d538edcc6a252a367caee1a43
= <=>
2016-11-02 11:42:38 +0000

    Added Horizontal Bar to Chart.js

---
+++ resources/views/chartjs/horizontalBar.blade.php
@@ -0,0 +1,57 @@
+@extends('charts::default')
+
+@if(!$model->customId)
+    @include('charts::_partials.container.canvas2')
+@endif
+
+<script type="text/javascript">
+var ctx = document.getElementById("{{ $model->id }}")
+var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: [
+            @foreach($model->labels as $label)
+                "{{ $label }}",
+            @endforeach
+        ],
+        datasets: [
+            {
+                label: "{{ $model->element_label }}",
+                backgroundColor: [
+                    @if($model->colors)
+                        @foreach($model->colors as $color)
+                            "{{ $color }}",
+                        @endforeach
+                    @else
+                        @foreach($model->values as $dta)
+                            "{{ sprintf('#%06X', mt_rand(0, 0xFFFFFF)) }}"
+                        @endforeach
+                    @endif
+                ],
+                data: [
+                    @foreach($model->values as $dta)
+                        "{{ $dta }}",
+                    @endforeach
+                ],
+            }
+        ]
+    },
+    options: {
+        responsive: ($model->responsive or !$model->width) ? 'true' : 'false',
+        maintainAspectRatio: false,
+        title: {
+            display: true,
+            text: '$model->title',
+            fontSize: 20,
+        },
+        scales: {
+            yAxes: [{
+                display: true,
+                ticks: {
+                    beginAtZero: true,
+                }
+            }]
+        }
+    }
+});
+</script>
---
+++ resources/views/chartjs/horizontalBar.multi.blade.php
@@ -0,0 +1,61 @@
+@extends('charts::default')
+
+@if(!$model->customId)
+    @include('charts::_partials.container.canvas2')
+@endif
+
+<script type="text/javascript">
+var ctx = document.getElementById("{{ $model->id }}")
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [
+            @foreach($model->labels as $label)
+                "{{ $label }}",
+            @endforeach
+        ],
+        datasets: [
+            $i = 0;
+            @foreach($model->datasets as $el => $ds)
+                {
+                    fill: true,
+                    label: "{{ $el }}",
+                    lineTension: 0.3,
+                    @if($model->colors and count($model->colors) > $i)
+                        borderColor: "{{ $model->colors[$i] }}",
+                        backgroundColor: "{{ $model->colors[$i] }}",
+                    @else
+                        $c = sprintf('#%06X', mt_rand(0, 0xFFFFFF))
+                        borderColor: "{{ $c }}",
+                        backgroundColor: "{{ $c }}",
+                    @endif
+                    data: [
+                        @foreach($ds['values'] as $dta)
+                            "{{ $dta }}",
+                        @endforeach
+                    ],
+                },
+                $i++;
+            }
+        ]
+    },
+    options: {
+        responsive: ($model->responsive or !$model->width) ? 'true' : 'false',
+        maintainAspectRatio: false,
+        title: {
+            display: true,
+            text: '$model->title',
+            fontSize: 20,
+        },
+        scales: {
+            yAxes: [{
+                display: true,
+                ticks: {
+                    beginAtZero: true,
+                }
+            }]
+        }
+    }
+});
+</script>
+
