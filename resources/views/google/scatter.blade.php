7c8d5c17efe8215d8871879dad63aa7a1271b1e3
Eugene Mah <eugenemah@gmail.com>
2017-04-14 10:59:11 -0400

    Add more options
    Add x/y title options.
    Remove height/width options because they're handled separately

--- resources/views/google/scatter.blade.php
+++ resources/views/google/scatter.blade.php
@@ -16,17 +16,18 @@
             @if($model->title)
                 title: "{!! $model->title !!}",
             @endif
+            @if($model->x_axis_title)
+            hAxis: {title: "{{ $model->x_axis_title }}"},
+            @endif
+            @if($model->y_axis_title)
+            vAxis: {title: "{{ $model->y_axis_title }}"},
+            @endif
             @if($model->colors)
                 colors: ["{{ $model->colors[0] }}"],
             @endif
-            @if($model->width)
-            width: {{ $model->width }},
-            @endif
-            @if($model->height)
-            height: {{ $model->height }},
-            @endif
             @if($model->background_color)
             backgroundColor: {{ $model->background_color }},
+            @endif
             @if(!$model->legend)
             legend: null
             @else
