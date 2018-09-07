d5645789c0fbca51b78b93d4bf2e1deb8bc216f9
HolmesOps <39928261+holmesops@users.noreply.github.com>
2018-09-07 10:29:03 -0500

    Support special characters on API's url. Console log received object.
    1. When using parameters on the API url specials characters are converted to html encode ("&"  converter to "&amp") during render. This create an invalid url. {!! !!} replacing {{  }} to support special characters.
    2. Preview of json object received from API on console to help debug.

--- src/Views/init.blade.php
+++ src/Views/init.blade.php
@@ -2,8 +2,9 @@ let {{ $chart->id }}_rendered = false;
 let {{ $chart->id }}_load = function () {
     if (document.getElementById("{{ $chart->id }}") && !{{ $chart->id }}_rendered) {
         @if ($chart->api_url)
-            fetch("{{ $chart->api_url }}")
+            fetch("{!! $chart->api_url !!}")
                 .then(data => data.json())
+                .then(data => console.log(data))
                 .then(data => { {{ $chart->id }}_create(data) });
         @else
             {{ $chart->id }}_create({!! $chart->formatDatasets() !!})
